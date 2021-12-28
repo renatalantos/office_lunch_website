0e6fba1a08fe02115ae3a432186cf41c35bc1a10
renatalantos <renatalantos@gmail.com>
2021-12-28 23:04:44 +0000

    Add search.html page as search did not look right in navbar

--- home/views.py
+++ home/views.py
@@ -6,3 +6,10 @@ def index(request):
     A view that returns the index page.
     """
     return render(request, 'home/index.html')
+
+
+def search(request):
+    """
+    A view that returns the search page.
+    """
+    return render(request, 'home/search.html')
