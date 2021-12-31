85e294e4fba6ac09132d63ef4c55523b5a93d347
renatalantos <renatalantos@gmail.com>
2021-12-31 20:54:27 +0000

    Add categories and products from fixtures

--- products/models.py
+++ products/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+class Product(models.Model):
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    product_number = models.CharField(max_length=4, null=True, blank=True)
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    option_grilled = models.BooleanField(default=False, null=True, blank=True)
+    lunch_deal = models.BooleanField(default=False, null=True, blank=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
