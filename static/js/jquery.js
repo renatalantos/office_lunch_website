88c507b1aee616f62c03b449d9b33a7e038b47f9
renatalantos <renatalantos@gmail.com>
2022-01-04 15:49:21 +0000

    Sort out issue with extra dropdown menu item containing further dropdown menu items. Sort items by lifestyle choices

--- static/js/jquery.js
+++ static/js/jquery.js
@@ -1,1 +1,25 @@
 $('.dropdown-toggle').dropdown()
+
+
+$(document).ready(function() {
+    $('.dropright div').on("click", function(e) {
+      e.stopPropagation();
+      e.preventDefault();
+  
+      if (!$(this).next('div').hasClass('show')) {
+        $(this).next('div').addClass('show');
+      } else {
+        $(this).next('div').removeClass('show');
+      }
+  
+    });
+  });
+
+
+$(document).ready(function(){
+  $('.dropdown-submenu a.test').on("click", function(e){
+    $(this).next('ul').toggle();
+    e.stopPropagation();
+    e.preventDefault();
+  });
+});
