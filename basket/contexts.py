c46e604ae27f6b557c8d43f9817aa5ba8fb41fd4
renatalantos <renatalantos@gmail.com>
2022-01-06 12:01:58 +0000

    Create contexts.py file to make basket content accessible throuhout all pages

---
+++ basket/contexts.py
@@ -0,0 +1,19 @@
+from decimal import Decimal
+from django.conf import settings
+
+def basket_contents(request):
+    basket_items = []
+    total = 0
+    product_count = 0
+    delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
+    final_total = delivery + total
+
+    context = {
+        'basket_items': basket_items,
+        'total': total,
+        'product_count': product_count,
+        'delivery': delivery,
+        'final_total': grand_total,
+    }
+
+    return context
--- office_lunches/settings.py
+++ office_lunches/settings.py
@@ -167,3 +167,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STANDARD_DELIVERY_PERCENTAGE = 10
