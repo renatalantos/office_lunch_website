1f0634710da9424469665ad35626c5ac95da1ebc
renatalantos <renatalantos@gmail.com>
2022-01-09 19:23:46 +0000

    Adjust basket/views.py by adding functions for updating and deleting basket items

--- basket/urls.py
+++ basket/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.view_basket, name='view_basket'),
     path('add/<item_id>', views.add_to_basket, name='add_to_basket'),
-
+    path('change/<item_id>', views.change_basket, name='change_basket'),
+    path('delete/<item_id>', views.delete_from_basket, name='delete_from_basket'),
 ]
--- basket/views.py
+++ basket/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, HttpResponse
 
 
 def view_basket(request):
@@ -33,3 +33,52 @@ def add_to_basket(request, item_id):
  
     request.session['basket'] = basket
     return redirect(redirect_url)
+
+
+def change_basket(request, item_id):
+    """Adjust the quantity of the specified product to the specified amount"""
+
+    quantity = int(request.POST.get('quantity'))
+    option_grill = None
+    if 'grilled' in request.POST:
+        option_grill = request.POST['grilled']
+    basket = request.session.get('basket', {})
+
+    if option_grilled:
+        if quantity > 0:
+            basket[item_id]['items_by_option_grill'][option_grill] = quantity
+        else:
+            del basket[item_id]['items_by_option_grill'][option_grill]
+            if not basket[item_id]['items_by_option_grill']:
+                basket.pop(item_id)
+    else:
+        if quantity > 0:
+            basket[item_id] = quantity
+        else:
+            basket.pop(item_id)
+
+    request.session['basket'] = basket
+    return redirect(reverse('view_basket'))
+
+
+def delete_from_basket(request, item_id):
+    """Delete item from the shopping basket"""
+
+    try:
+        option_grill = None
+        if 'grilled' in request.POST:
+            option_grill = request.POST['grilled']
+        basket = request.session.get('basket', {})
+
+        if option_grill:
+            del basket[item_id]['items_by_option_grill'][option_grill]
+            if not basket[item_id]['items_by_option_grill']:
+                basket.pop(item_id)
+        else:
+            basket.pop(item_id)
+
+        request.session['basket'] = basket
+        return HttpResponse(status=200)
+
+    except Exception as e:
+        return HttpResponse(status=500)
