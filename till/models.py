398f2e0491f0702362ba944421b02cfbd2a99e73
renatalantos <renatalantos@gmail.com>
2022-01-11 15:33:28 +0000

    Create model for till app

--- till/models.py
+++ till/models.py
@@ -1,10 +1,11 @@
 import uuid
-
+import datetime
 from django.db import models
 from django.db.models import Sum
 from django.conf import settings
 from django.utils import timezone
 from django.core.exceptions import ValidationError
+from django.core.validators import RegexValidator
 
 from products.models import Product
 
@@ -17,11 +18,31 @@ class Order(models.Model):
     town_or_city = models.CharField(max_length=40, null=False, blank=False)
     street_address1 = models.CharField(max_length=80, null=False, blank=False)
     street_address2 = models.CharField(max_length=80, null=True, blank=True)
-    postcode = models.CharField(max_length=20, null=True, blank=True)
+    postcode = models.CharField(max_length=20,
+                                null=True,
+                                blank=True,
+                                validators=[RegexValidator(
+                                            regex='^(?:^[V-]\
+                                                  [0-9]{2})[ -]?[FHKNPRTV-Y]\
+                                                  {4}$',
+                                            message='Sorry, you are outside of\
+                                                     our delivery range.\
+                                                     We only deliver in the\
+                                                     Limerick-Shannon area.',
+                                            )],
+                                )
     county = models.CharField(max_length=80, null=True, blank=True)
-    date = models.DateTimeField(auto_now_add=True)
-    delivery_date = models.DateField()
-    
+    date_of_order = models.DateTimeField(auto_now_add=True)
+    cutoff = datetime.time(11, 59)
+
+    def validate_date_of_order(date_of_order):
+        delivery_date = models.DateTimeField()
+        if date_of_order > date_of_order.cutoff \
+                and delivery_date \
+                == datetime.today():
+            raise ValidationError("Order placed too late\
+                                   for today's delivery.")
+
     def validate_delivery_date(delivery_date):
         """
         Function to validate date so that
@@ -30,11 +51,20 @@ class Order(models.Model):
         if delivery_date < timezone.now():
             raise ValidationError("Delivery date cannot be in the past")
 
-    delivery_cost = models.DecimalField(max_digits=6, decimal_places=2, null=False, default=0)
-    order_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
-    final_total = models.DecimalField(max_digits=10, decimal_places=2, null=False, default=0)
+    delivery_charge = models.DecimalField(max_digits=6,
+                                          decimal_places=2,
+                                          null=False,
+                                          default=0)
+    order_total = models.DecimalField(max_digits=10,
+                                      decimal_places=2,
+                                      null=False,
+                                      default=0)
+    final_total = models.DecimalField(max_digits=10,
+                                      decimal_places=2,
+                                      null=False,
+                                      default=0)
 
-    def _generate_order_number(self):
+    def _create_order_number(self):
         """
         Generate a random, unique order number using UUID
         """
@@ -42,12 +72,12 @@ class Order(models.Model):
 
     def update_total(self):
         """
-        Update grand total each time a line item is added,
-        accounting for delivery costs.
+        Update final total each time a component item is added,
+        accounting for delivery charges.
         """
-        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum']
-        self.delivery_cost = self.order_total * settings.STANDARD_DELIVERY_PERCENTAGE / 100
-        self.final_total = self.order_total + self.delivery_cost
+        self.order_total = self.componentitems.aggregate(Sum('componentitem_total'))['componentitem_total__sum']
+        self.delivery_charge = self.order_total * settings.STANDARD_DELIVERY_PERCENTAGE / 100
+        self.final_total = self.order_total + self.delivery_charge
         self.save()
 
     def save(self, *args, **kwargs):
@@ -56,27 +86,27 @@ class Order(models.Model):
         if it hasn't been set already.
         """
         if not self.order_number:
-            self.order_number = self._generate_order_number()
+            self.order_number = self._create_order_number()
         super().save(*args, **kwargs)
 
     def __str__(self):
         return self.order_number
 
 
-class OrderLineItem(models.Model):
-    order = models.ForeignKey(Order, null=False, blank=False, on_delete=models.CASCADE, related_name='lineitems')
+class OrderComponentItem(models.Model):
+    order = models.ForeignKey(Order, null=False, blank=False, on_delete=models.CASCADE, related_name='componentitems')
     product = models.ForeignKey(Product, null=False, blank=False, on_delete=models.CASCADE)
-    product_size = models.CharField(max_length=2, null=True, blank=True) # XS, S, M, L, XL
+    grilled = models.CharField(max_length=11, null=True, blank=True)
     quantity = models.IntegerField(null=False, blank=False, default=0)
-    lineitem_total = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False, editable=False)
+    componentitem_total = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False, editable=False)
 
     def save(self, *args, **kwargs):
         """
-        Override the original save method to set the lineitem total
+        Override the original save method to set the componentitem total
         and update the order total.
         """
-        self.lineitem_total = self.product.price * self.quantity
+        self.componentitem_total = self.product.price * self.quantity
         super().save(*args, **kwargs)
 
     def __str__(self):
-        return f'SKU {self.product.sku} on order {self.order.order_number}'
+        return f'Product nr {self.product.product_number} on order {self.order.order_number}'
