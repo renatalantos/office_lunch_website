82393ff551bea400f9fcb581b95f2d8894002166
renatalantos <renatalantos@gmail.com>
2022-01-15 23:03:21 +0000

    Add body and subject for confirmation emails in txt format

--- office_lunches/settings.py
+++ office_lunches/settings.py
@@ -194,3 +194,4 @@ STRIPE_CURRENCY = 'eur'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
+DEFAULT_FROM_EMAIL = 'officelunches@example.com'
