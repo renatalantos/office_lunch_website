289c4a0af04e23b607ff8231007525f3fae37cda
renatalantos <renatalantos@gmail.com>
2022-01-23 08:22:29 +0000

    Corrected html code in contactus app by adding name attribute to textarea

--- reviews/views.py
+++ reviews/views.py
@@ -59,11 +59,11 @@ def edit_review(request, review_id):
     title = review.title
    
     current_user = CustomerProfile.objects.get(user=request.user)
-    if review.user != current_user:
-        messages.error(request, 'You are tring to edit a review by someone else.')
-        return redirect(reverse('product_detail', args=[product.id]))
-    else:
-        messages.info(request, 'You are editing your review')
+    # if review.user != current_user:
+    #     messages.error(request, 'You are tring to edit a review by someone else.')
+    #     return redirect(reverse('product_detail', args=[product.id]))
+    # else:
+    #     messages.info(request, 'You are editing your review')
     if request.method == "POST":
         review_form = ReviewForm(request.POST, instance=review)
 
@@ -119,6 +119,64 @@ def delete_review(request, review_id):
     context = {
         'review_form': review_form,
         'review': review,
-        'product': product,
+       
     }
     return render(request, template, context)
+
+
+# @login_required
+# def add_review(request, product_id):
+#     """
+#     A view to add a review to a product
+#     """
+#     product = get_object_or_404(Product, pk=product_id)
+#     if request.method == 'POST':
+#         review_form = ReviewForm(request.POST)
+
+#         if review_form.is_valid():
+#             already_reviewed = ProductReview.objects.filter(product=product,
+#                                                      user=request.user)
+#             if not already_reviewed:
+#                 ProductReview.objects.create(
+#                         product=product,
+#                         user=request.user,
+#                         product_rating=request.POST['product_rating'],
+#                         review_text=request.POST['review_text'],
+#                 )
+#                 messages.info(request, 'Successfully added a review!')
+#             else:
+#                 messages.error(request, 'You have already reviewed '
+#                                         'this product!')
+#             return redirect(reverse('product_detail', args=[product.id]))
+
+#         messages.error(request, 'Failed to add product review')
+#     messages.error(request, 'Invalid Method.')
+#     return redirect(reverse('product_detail', args=[product.id]))
+
+# @login_required
+# def edit_review(request, review_id):
+#     """
+#     Edit exisiting review
+#     """
+#     review = get_object_or_404(Review, pk=review_id)
+#     if request.method == 'POST':
+#         form = ReviewForm(request.POST, instance=review)
+
+#         if form.is_valid():
+#             # review_form.save()
+#             messages.info(request, 'Successfully updated your review!')
+#             return redirect(reverse('product_detail', args=[review.id]))
+#         else:
+#             messages.error(request, 'Failed to update product review')
+#     else:
+#         form = ReviewForm(instance=review)
+#         messages.info(request, 'You are editing your review for ' +
+#                       f'{review.product.name}')
+
+#     template = 'products/edit_review.html'
+#     context = {
+#         'form': form,
+#         'review': review,
+#     }
+
+#     return render(request, template, context)
