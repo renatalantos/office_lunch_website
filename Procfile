23d9df7cae5dc4ae0ec6565ed007b3914ca2c193
renatalantos <renatalantos@gmail.com>
2022-01-23 16:50:51 +0000

    Deployment after creating Procfile

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn office_lunches.wsgi:application
--- office_lunches/settings.py
+++ office_lunches/settings.py
@@ -28,6 +28,8 @@ SECRET_KEY = 'django-insecure-e@p&0fph@)1w(g*#$&wqczr#$o@zvq*ehn!32qeb9^3nc3o!7*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+ALLOWED_HOSTS = ['renatalantos-office-lunches.herokuapp.com', 'localhost']
+
 ALLOWED_HOSTS = []
 
 
@@ -134,12 +136,17 @@ MESSAGE_TAGS = {
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 
