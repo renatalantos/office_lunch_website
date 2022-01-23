c6afc93250ff0faf3f95f4d61a0dcde873befb07
renatalantos <renatalantos@gmail.com>
2022-01-23 18:10:14 +0000

    Add secret key

--- office_lunches/settings.py
+++ office_lunches/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-e@p&0fph@)1w(g*#$&wqczr#$o@zvq*ehn!32qeb9^3nc3o!7*'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['renatalantos-office-lunches.herokuapp.com', 'localhost']
 
